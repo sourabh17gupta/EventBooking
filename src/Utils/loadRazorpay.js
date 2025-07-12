7e48ea81f9d3aa5fad21034ba245c00c129514bf
sourabh17gupta <mkktlguptas@gmail.com>
2025-07-12 20:03:17 +0530

    Update loadRazorpay.js

--- src/Utils/loadRazorpay.js
+++ src/Utils/loadRazorpay.js
@@ -1,10 +1,15 @@
 // utils/loadRazorpay.js
-exports.loadRazorpayScript = () => {
-    return new Promise((resolve) => {
-        const script = document.createElement("script");
-        script.src = "https://checkout.razorpay.com/v1/checkout.js";
-        script.onload = () => resolve(true);
-        script.onerror = () => resolve(false);
-        document.body.appendChild(script);
-    });
+export const loadRazorpayScript = () => {
+  return new Promise((resolve) => {
+    if (document.getElementById("razorpay-sdk")) {
+      return resolve(true);
+    }
+
+    const script = document.createElement("script");
+    script.id = "razorpay-sdk";
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    script.onload = () => resolve(true);
+    script.onerror = () => resolve(false);
+    document.body.appendChild(script);
+  });
 };
